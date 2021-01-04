2077f591cd1634ed1dff4ef5e3c584a3d165886c
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-01-03 20:16:00 -0500

    👌 IMPROVE: Minor Fixes

--- DT-JMeter.groovy
+++ DT-JMeter.groovy
@@ -9,7 +9,7 @@ def lsn
 try {
 	lsn = (args.length > 0) ? args[0] : "No name for Test Plan" //To Configure Test Plan name, add ${__TestPlanName} in Parameters as first argument
 	ltn = vars.get("LoadTestName") ?: "No name for Load Test" //To Configure Load Test Name, add a variable `LoadTestName` and the value in Test Plan or User Defined Variables
-	dtheaders = "SI="+ si + ";LSN="+ lsn + ";TSN=" + tsn + ";LTN=" + ltn + ";VU=" + vu + ";";
+	dtheaders = "SI=" + si + ";LSN="+ lsn + ";TSN=" + tsn + ";LTN=" + ltn + ";VU=" + vu + ";"
 	sampler.headerManager.removeHeaderNamed('X-Dynatrace-Test')
 	sampler.headerManager.add(new Header('X-Dynatrace-Test', dtheaders))
 }
